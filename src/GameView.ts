6081bf466fe00f3d35d3be654c8a339160b84a11
kusebingtang <kusebintang@163.com>
2019-04-29 17:08:19 +0800

    完成游戏倒计时功能

--- src/GameView.ts
+++ src/GameView.ts
@@ -7,6 +7,9 @@ class GameView extends ui.GameUIUI{
     private moleNum:number = 9;  //9个树洞
     constructor(){
         super();
+
+        this.timeBar.value = 1.0  // 设置倒计时 时间progressbar
+
         this.moles = new Array<Mole>();
         for (var index = 0; index < this.moleNum; index++) {
             var box: Laya.Box = this.getChildByName("item"+index) as Laya.Box;
@@ -17,13 +20,26 @@ class GameView extends ui.GameUIUI{
 
             this.moles.push(mole);
         }
-        Laya.timer.loop(2000,this,this.onLoop);
+        Laya.timer.loop(1000,this,this.onLoop);
         
     }
 
     onLoop(): void {
+        
+        this.timeBar.value -= (1/90);
+        if(this.timeBar.value<=0){
+            this.gameOver();
+            return;
+        }
+
         var index:number = Math.floor(Math.random()*this.moleNum)
         var mole = this.moles[index];
         mole.show();
     }
+
+    gameOver(): void {
+        Laya.timer.clear(this,this.onLoop);
+        console.log("游戏结束！！！！")
+    }
+
 }
