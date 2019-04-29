69aed4e5d13eb81816d79e344ab34b49ec526df3
kusebingtang <kusebintang@163.com>
2019-04-29 14:53:50 +0800

    修改布局文件，9个树洞添加地鼠

--- src/GameView.ts
+++ src/GameView.ts
@@ -6,13 +6,12 @@ class GameView extends ui.GameUIUI{
     private mole:Mole;
     constructor(){
         super();
-
-        this.mole = new Mole(this.normal,this.hit,21);
+        //this.mole = new Mole(this.normal,this.hit,21);
         Laya.timer.loop(2000,this,this.onLoop);
         
     }
 
     onLoop(): void {
-        this.mole.show();
+        //this.mole.show();
     }
 }
